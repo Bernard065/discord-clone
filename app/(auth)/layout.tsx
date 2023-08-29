10ca116706b9dd9d351e2ecaa0db7e57e37bd285
+ <+>
2023-08-29 04:04:57 +0300

    implementing authentication for sign-up and sign-in

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -5,7 +5,9 @@ const AuthLayout = ({
     children: React.ReactNode
   }) => {
   return (
-    <div>{children}</div>
+    <div className="h-full flex items-center justify-center">
+        {children}
+    </div>
   )
 }
 
